5055f2a681bed30b5e59db2bf4dfc3e087018f46
pavelstudenik@gmail.com <pavelstudenik@gmail.com@92f6db88-26b0-51a2-1dd7-9257b8258dd1>
2012-11-20 23:57:23 +0000

    working on video play/pause

--- trunk/src/apps.h
+++ trunk/src/apps.h
@@ -1,14 +1,32 @@
 
 #include <gtk/gtk.h>
 #include "freenect.h"
+#include <cv.h>
+#include <highgui.h> // contain function for loading video
 
 #define MODE_PLAY 1
 #define MODE_PAUSE 2
 #define MODE_REC 3
 
+#define TIMER 30
+#define C_EXTERN    extern "C"
+
+gboolean 
+on_timer (gpointer arg);
+
+C_EXTERN gboolean
+on_draw_video (GtkWidget * object, GdkEvent * eev, gpointer data);
+
 class App {
     private:
-        short mode;
+        gint mode;
+        // limits of frames
+        gint frames_max;
+        gint frames_min;
+
+        CvCapture *capture;
+        IplImage *frame_rgb;
+        IplImage *frame_depth;
 
     public:
         gint n_frame;
@@ -18,11 +36,20 @@ class App {
         GtkWidget   * messagedialog1;
         GtkWidget   * drawarea;
         GtkScale    * scale;
+        GtkButton   * button3;
+        GtkAdjustment * adjustment;
+        freenect    * kinect;
 
         App(char * file);
         ~App();
 
-        void set_mode(short _mode);
+        void set_mode(gint _mode);
+        gint get_mode();
+        void load_video(char * file);
+        gboolean next_frame();
+        void play();
+
+        IplImage * get_image_rgb();
 };
 
 
